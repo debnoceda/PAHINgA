5d371e35fccb0c602f826ebba67117560a18e0fc
Dave Noceda <deibeunoceda26@gmail.com>
2025-05-25 00:23:45 +0800

    Update url to include Journal, Insights, and MoodStats

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework.routers import DefaultRouter
-from api import views
-
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
