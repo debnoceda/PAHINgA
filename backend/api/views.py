f8edd4b40b7221bb84d32d57f4a1273772454e24
Dave Noceda <deibeunoceda26@gmail.com>
2025-05-24 23:53:57 +0800

    Update urls.py to access JWT Token, and views

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,13 +9,3 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-
-    # def get_permissions(self):
-    #     if self.action == 'create':
-    #         return [AllowAny()]
-    #     return super().get_permissions()
-
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     if serializer.is_valid():
-    #         user = serializer.save()
