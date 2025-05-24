066102788f5f62441555c1ef95d599e762cee304
Dave Noceda <deibeunoceda26@gmail.com>
2025-05-25 00:18:50 +0800

    Add ViewSet for Journal, MoodStats, and Insights

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Journal, MoodStats, Insights
 from rest_framework import viewsets
-from .serializers import UserSerializer
+from .serializers import UserSerializer, JournalSerializer, MoodStatsSerializer, InsightsSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
@@ -9,3 +10,20 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class JournalViewSet(viewsets.ModelViewSet):
+    serializer_class = JournalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Journal.objects.filter(user=self.request.user)
+
+class MoodStatsViewSet(viewsets.ModelViewSet):
+    queryset = MoodStats.objects.all()
+    serializer_class = MoodStatsSerializer 
+    permission_classes = [IsAuthenticated]
+
+class InsightsViewSet(viewsets.ModelViewSet):
+    queryset = Insights.objects.all()
+    serializer_class = InsightsSerializer
+    permission_classes = [IsAuthenticated]
