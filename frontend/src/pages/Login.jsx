4b4ef8aca4be85d73d42df0584c5ec5cb353f899
Dave Noceda <deibeunoceda26@gmail.com>
2025-06-02 22:28:41 +0800

    Add Login CSS layout and other UI elements

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,23 @@
 import LoginRegisterForm from '../components/LoginRegisterForm';
+import loginImage from '../assets/login.png';
+import '../styles/Login.css';
 
 function Login() {
-    return <LoginRegisterForm method='login' />
+    return <body className='login-body'>
+        <div className='login-container'>
+            <h1 className='title'>PAHINgA</h1>
+            <div className='login-form-container'>
+                <div className='text-image-container'>
+                    <h1>Welcome Back</h1>
+                    <p>Take a moment. Your thoughts are safe here.</p>
+                    <img src={loginImage} alt="Login" />
+                </div>
+                <LoginRegisterForm method='login' />
+            </div>
+            
+        </div>
+    </body>
+    
 }
 
 export default Login;
