88f167ecd19e567a7328547fb8fff51d384553ff
Lenny <lrilandag@up.edu.ph>
2025-06-03 03:45:34 +0800

    Add initial piechart.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import NavigationBar from '../components/NavigationBar';
 import FloatingActionButton from '../components/FloatingActionButton';
+import PieChart from '../components/PieChart';
+
+const sampleData = [
+  { name: 'Happiness', value: 90 },
+  { name: 'Anger', value: 12 },
+  { name: 'Fear', value: 34 },
+  { name: 'Disgust', value: 53 },
+  { name: 'Sadness', value: 98 },
+];
 
 function Home() {
     return (
         <div>
             <NavigationBar />
             <h1>Home</h1>
+            <PieChart width={250} height={250} data={sampleData} emotionCode={1}/>
             <FloatingActionButton />
         </div>
     );
