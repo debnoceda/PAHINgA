5ba7d51bd61127cafc109816fe4b48b8140d0de4
Zayne Peladas <123078039+BeauBeannie@users.noreply.github.com>
2025-06-03 02:31:11 +0800

    Connect Get Started button to Home Page

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -2,12 +2,22 @@ import welcomeImage from '../assets/WelcomePic2.png'; // adjust path as needed
 import '../styles/Welcome.css'; // optional CSS
 import Button from '../components/Button';
 
-const Welcome = ({ username, onGetStarted }) => {
+import { useNavigate } from "react-router-dom";
+import React from 'react';
+
+
+const Welcome = () => {
+  const navigate = useNavigate();
+
+  const handleGetStarted = () => {
+    navigate("/");
+  };
+
   return (
     <div className="welcome-container">
       <img src={welcomeImage} alt="Welcome" className="welcome-image" />  
       <h1 className="welcome-title">Welcome to Our App!</h1>
-      <Button type='medium-compact' className="get-started-button" onClick={onGetStarted}>
+      <Button type='medium-compact' className="get-started-button" onClick={() => handleGetStarted()}>
         Get Started
       </Button>
     </div>
