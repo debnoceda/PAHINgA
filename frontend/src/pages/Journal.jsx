260e711ffdb553535cfe61dcf1f757d9bedab054
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-06-04 01:21:53 +0800

    Add journal page header

--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import { useEffect } from 'react';
 import NavigationBar from '../components/NavigationBar';
 import FloatingActionButton from '../components/FloatingActionButton';
 import Card from '../components/Card';
 import JournalList from '../components/JournalList';
 import { useUser } from '../context/UserContext';
+import '../styles/Journal.css';
+import { Icon } from '@iconify/react';
 
 function Journal() {
     const { fetchJournals } = useUser();
+    const [search, setSearch] = React.useState('');
 
     useEffect(() => {
         fetchJournals();
@@ -15,6 +19,33 @@ function Journal() {
     return (
         <div>
             <NavigationBar />
+            <div className="journal-header">
+                <div className='journal-header-content'>
+                    <p className="journal-title">
+                        Your Journal Journey
+                    </p>
+                    <p className="journal-subtitle small-text">
+                        A record of your reflections, one day at a time.
+                    </p>
+                </div>
+                <div className="journal-searchbar">
+                    <div className="journal-search-input-wrapper">
+                        <span className="journal-search-icon">
+                            <Icon icon="material-symbols:search-rounded" width="22" height="22" />
+                        </span>
+                        <input
+                            type="text"
+                            placeholder="Search title, content..."
+                            className="custom-input journal-search-input small-text"
+                            value={search}
+                            onChange={e => setSearch(e.target.value)}
+                        />
+                    </div>
+                    <button className="journal-filter-icon">
+                        <Icon icon="stash:filter-solid" width="24" height="24" />
+                    </button>
+                </div>
+            </div>
             <JournalList />
             <FloatingActionButton />
         </div>
