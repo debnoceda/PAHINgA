7b350e5d8c6176196e9553136cbf035d71ee52d0
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2025-06-03 22:15:24 +0800

    Add journal list in journal page

--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import NavigationBar from '../components/NavigationBar';
 import FloatingActionButton from '../components/FloatingActionButton';
+import Card from '../components/Card';
+import JournalList from '../components/JournalList';
+import { useUser } from '../context/UserContext';
 
 function Journal() {
+    const { fetchJournals } = useUser();
+
+    useEffect(() => {
+        fetchJournals();
+    }, [fetchJournals]);
+
     return (
         <div>
             <NavigationBar />
-            <h1>Journal</h1>
+            <JournalList />
             <FloatingActionButton />
         </div>
     );
