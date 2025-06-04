e72fb342f17cd3f78e09ca93764dee9e4b383a5b
Lenny <lrilandag@up.edu.ph>
2025-06-04 19:04:04 +0800

    Add initial home style

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import NavigationBar from '../components/NavigationBar';
 import FloatingActionButton from '../components/FloatingActionButton';
 import PieChart from '../components/PieChart';
+import Pet from '../components/Pet';
+import '../styles/Home.css';
 
 const sampleData = [
   { name: 'Happiness', value: 90 },
@@ -10,12 +12,35 @@ const sampleData = [
   { name: 'Sadness', value: 98 },
 ];
 
+const yourBackendValue = 0;
+
 function Home() {
     return (
         <div>
             <NavigationBar />
-            <h1>Home</h1>
-            <PieChart width={250} height={250} data={sampleData} emotionCode={1}/>
+            <div className="home-container">
+                <div className="home-pet-section">
+                    <Pet emotionCode={yourBackendValue} />
+                    <div className="mallow-pet-label">Mallow Pet</div>
+                </div>
+                <div className="home-content-top">
+                    <div className="home-calendar-section">
+                        {/* Calendar Placeholder */}
+                        <div className="calendar-box">
+                            <p>Calendar</p>
+                        </div>
+                    </div>
+                    <div className="home-pie-section">
+                        <PieChart data={sampleData} emotionCode={1}/>
+                    </div>
+                </div>
+                <div className="home-content-bottom">
+                    
+                    <div className="home-recent-section card">
+                        <div className="recent-entries-box-header">Recent Entries</div>
+                    </div>
+                </div>
+            </div>
             <FloatingActionButton />
         </div>
     );
